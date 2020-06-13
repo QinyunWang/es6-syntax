b37a70b47de5012ae9c18c3924ec7451bc88d713
Qinyun Wang <oojbjoo@gmail.com>
2020-06-14 00:14:23 +0800

    Complete the practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
-export { parseData };
+const parseData = input => {
+  var output = []
+  const { data, column } = input
+  for (const value of data) {
+    var item = {}
+    for (const [index, field] of value.entries()) {
+      item[column[index].name] = field
+    }
+    output.push(item)
+  }
+  return output
+}
+
+export { parseData }
