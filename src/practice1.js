51220c36ec878082d30154ebfea5b23728ae9cc5
Qinyun Wang <oojbjoo@gmail.com>
2020-06-14 01:40:49 +0800

    Complete the practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,9 +1,9 @@
 const parseData = input => {
   var output = []
   const { data, column } = input
-  for (const value of data) {
-    var item = {}
-    for (const [index, field] of value.entries()) {
+  for (let value of data) {
+    let item = {}
+    for (let [index, field] of value.entries()) {
       item[column[index].name] = field
     }
     output.push(item)
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,24 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+  let numberOfSections = sections.length 
+  var slicedSubArray = []
+
+  // slice the items array to sub arrays which will add sections to middle
+  slicedSubArray.push(items.slice(0, sections[0].index))
+  for (let i=0; i<=numberOfSections-2; i++) {
+    slicedSubArray.push(items.slice(sections[i].index, sections[i+1].index))
+  }
+  slicedSubArray.push(items.slice(sections[numberOfSections-1].index, items.length))
+
+  let result = []
+  for (let [index, section] of sections.entries()) {
+    if (slicedSubArray[index].length!=0) {
+      result.push(...slicedSubArray[index])
+    }
+    result.push(section.content)
+  }
+  if (slicedSubArray[slicedSubArray.length-1].length!=0) {
+    result.push(...slicedSubArray[slicedSubArray.length-1])
+  }
+  return result
+}
+export { inject }
